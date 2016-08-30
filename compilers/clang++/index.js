e7255f53a26983cbec322d195c473873ed52fe8e
Jake Renzella <j@kerenzella.com>
2016-08-30 18:31:31 +1000

    REFACTOR: Finalise new build

--- compilers/clang++/index.js
+++ compilers/clang++/index.js
@@ -1,9 +1,9 @@
-const os = require('os')
 const utils = require('../../utils')
+const {homedir} = require('os')
 
 execute = function (argv, callback) {
-  const cppBuild = `${os.homedir()}/.splashkit/splashkit-macos/compilers/clang++`
-  const splashkitLib = `${os.homedir()}/.splashkit/splashkit-macos/lib`
+  const cppBuild = `${homedir()}/.splashkit/splashkit-macos/compilers/clang++`
+  const splashkitLib = `${homedir()}/.splashkit/splashkit-macos/lib`
 
   const userArgs = utils.argsToString(argv['original_string'])
   const clangArgs = `-L${splashkitLib} -I ${cppBuild}/include -l splashkit -framework IOKit -framework ForceFeedback -framework CoreFoundation -framework Cocoa -framework Carbon -framework AudioUnit -framework AudioToolbox -framework CoreAudio -framework CoreVideo -lcurl`
