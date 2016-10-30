fccffa392ba8b3bab18da93956cf51b4abde0aea
Andrew Cain <acain@swin.edu.au>
2016-10-31 10:09:16 +1100

    FIX: Update build details for fpc

--- compilers/fpc/index.js
+++ compilers/fpc/index.js
@@ -1,15 +1,15 @@
 const utils = require('../../utils')
 const home = process.env.HOME
 
-execute = function (path, callback) {
-  const cppBuild = `${home}/.splashkit/splashkit-macos/compilers/clang++`
-  const sklibs = `${home}/.splashkit/splashkit-macos/lib`
-  const skPasUnits = `${home}/.splashkit/splashkit-macos/compilers/fpc`
+execute = function (argv, callback) {
+  const cppBuild = `${home}/.splashkit/compilers/clang++`
+  const sklibs = `${home}/.splashkit/lib`
+  const skPasUnits = `${home}/.splashkit/compilers/fpc`
 
   const userArgs = utils.argsToString(argv['original_string'])
-  const fpcArgs = `-Fu${skPasUnits} -k-llibSplashKit.dylib -k"-L${sklibs.static}" -k"-rpath @loader_path -rpath ${sklibs.dynamic} -rpath /usr/local/lib"`
+  const fpcArgs = `-S2 -Fu${skPasUnits} -k"-llibSplashKit.dylib" -k"-L${sklibs}" -k"-rpath @loader_path -rpath ${sklibs} -rpath /usr/local/lib"`
 
-  utils.runCommand(`clang++ ${userArgs} ${fpcArgs}`, function (err, data) {
+  utils.runCommand(`ppcx64 ${userArgs} ${fpcArgs}`, function (err, data) {
       if (err) {
           callback(err)
       } else {
