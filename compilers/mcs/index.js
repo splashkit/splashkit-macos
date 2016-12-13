20af9c89750151b2cb8150485a1a2fbfea064ead
Andrew Cain <macite@gmail.com>
2016-12-13 11:59:17 +1100

    NEW: Add index for building C# via skm

---
+++ compilers/mcs/index.js
@@ -0,0 +1,22 @@
+const utils = require('../../utils')
+const home = process.env.HOME
+
+execute = function (argv, callback) {
+  const sklibs = `${home}/.splashkit/lib`
+  const skCSharpCode = `${home}/.splashkit/compilers/mcs`
+
+  const userArgs = utils.argsToString(argv['original_string'])
+  const mcsArgs = `"${skCSharpCode}"`
+
+  utils.runCommand(`mcs ${userArgs} ${mcsArgs}`, function (err, data) {
+      if (err) {
+          callback(err)
+      } else {
+          callback()
+      }
+  })
+}
+
+ module.exports = {
+  execute : execute
+}
