a18b157b4e8310bb847fa15059c623b7d369f78a
Jake Renzella <j@kerenzella.com>
2016-12-14 16:37:30 +1100

    .gitignore is now working

--- commands/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-let commandDir = __dirname
-let cmds = fs.readdirSync(commandDir)
-             .filter(cmd => cmd != 'index.js' && cmd != '.DS_Store')
-             .map(cmd => [cmd, require(commandDir + '/' + cmd)] )
-
-module.exports = new Map(cmds)
