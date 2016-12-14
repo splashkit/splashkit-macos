bb790e4d3d0ce361f7fa671c2fd00ae0fb7f0e25
Andrew Cain <macite@gmail.com>
2016-12-14 17:18:59 +1100

    NEW: Add dotnet command

---
+++ commands/dotnet/index.js
@@ -0,0 +1,34 @@
+const utils = require('../../utils')
+const home = process.env.HOME
+
+execute = function (argv, callback) {
+  const sklibs = `${home}/.splashkit/lib`
+
+  const userArgs = utils.argsToString(argv['original_string'])
+  const dotnetArgs = `DYLD_LIBRARY_PATH="${sklibs}"`
+  const skCSharpCode = `${home}/.splashkit/commands/mcs/SplashKit.cs`
+  const skCSharpProgram = `${home}/.splashkit/commands/dotnet/Program.cs`
+
+  utils.runCommand(`${dotnetArgs} dotnet ${userArgs}`, function (err, data) {
+      if (err) {
+          callback()
+      } else {
+          if ( userArgs == "new" ) {
+              utils.runCommand(`mkdir -p ./lib; ln -s "${skCSharpCode}" ./lib/SplashKit.cs; cp "${skCSharpProgram}" .`, function (err1, data) {
+                  if (err1) {
+                      callback(null, "I couldn't add in the SplashKit library... 😟 -- ${err1}")
+                  } else {
+                      callback(null, '🎉  dotnet command run successfully 🎉')
+                  }
+              })
+          }
+          else {
+            callback(null, '🎉  dotnet command run successfully 🎉')
+          }
+      }
+  })
+}
+
+ module.exports = {
+  execute : execute
+}
