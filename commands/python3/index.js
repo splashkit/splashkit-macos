cf03eafc990b3328f667a905bfed8701939258a4
Jake Renzella <j@kerenzella.com>
2017-02-08 16:00:07 +1100

    FIX: Add assignment to DYLD path

--- commands/python3/index.js
+++ commands/python3/index.js
@@ -2,12 +2,12 @@ const utils = require('../../utils')
 const home = process.env.HOME
 
 execute = function (argv, callback) {
-  const sklibs = `${home}/.splashkit/lib`
+  const sklibs = `"${home}/.splashkit/lib"`
   const skPythonLibrary = `${home}/.splashkit/commands/python3`
 
   const userArgs = utils.argsToString(argv['original_string'])
 
-  utils.runCommand(`python3 ${userArgs} DYLD_LIBRARY_PATH=${skPythonLibrary}`, function (err, data) {
+  utils.runCommand(`DYLD_LIBRARY_PATH=${skPythonLibrary} python3 ${userArgs}`, function (err, data) {
     if (err) {
       callback(err)
     } else {
