224c445198bb71dedc5204130e544168bb6a429e
Andrew Cain <macite@gmail.com>
2017-02-09 14:13:30 +1100

    New splashkit mac -- network changes and gpp

---
+++ commands/gpp/index.js
@@ -0,0 +1,25 @@
+const utils = require('../../utils')
+const home = process.env.HOME
+
+execute = function (argv, callback) {
+  const cppBuild = `${home}/.splashkit/commands/g++`
+  const sklibs = {
+    static: `${home}/.splashkit/commands/g++/lib`,
+    dynamic: `${home}/.splashkit/lib`
+  }
+
+  const userArgs = utils.argsToString(argv['original_string'])
+  const gppArgs = `-L${sklibs.dynamic} -lSplashKit -L${sklibs.static} -lSplashKitCpp -I ${cppBuild}/include -rpath @loader_path -rpath ${sklibs.dynamic} -rpath /usr/local/lib`
+
+  utils.runCommand(`g++ ${gppArgs} ${userArgs}`, function (err, data) {
+      if (err) {
+          callback(err)
+      } else {
+          callback(null, '🎉  Successfully compiled with g++ 🎉')
+      }
+  })
+}
+
+ module.exports = {
+  execute: execute
+}
